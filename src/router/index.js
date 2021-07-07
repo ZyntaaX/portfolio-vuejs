411b08e9025ae4af19c76f25b64427503d8050a9
Rasmus Svanberg <svra18cy@student.ju.se>
2021-07-07 18:06:57 +0200

    Test with HashHistory instead

--- src/router/index.js
+++ src/router/index.js
@@ -43,7 +43,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(), // createWebHashHistory(),
+  history: createWebHashHistory(), // createWebHistory(),
   routes
 })
 
