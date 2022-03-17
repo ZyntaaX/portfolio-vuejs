556cfc804ddbe270c70205c89c46187124ba3455
Rasmus Svanberg <svra18cy@student.ju.se>
2022-03-17 12:34:15 +0100

    Test

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
-import { createRouter, createWebHashHistory /* createWebHistory */ } from 'vue-router';
+// import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/home/home.view.vue';
 import AboutView from '@/views/about/about.view.vue';
 import ContactView from '@/views/contact/contact.view.vue';
@@ -44,7 +45,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(/* process.env.BASE_URL */),
+  history: createWebHistory(/* process.env.BASE_URL */),
   routes,
 });
 
